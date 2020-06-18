a40812f179bdf985d2501a93d17ff107168a5482
ShpakovNikita <shpakovnikita1998@gmail.com>
2020-06-18 14:35:07 +0300

    metal basic example created

---
+++ src/main.mm
@@ -0,0 +1,56 @@
+#import "Foundation/Foundation.h"
+#import <Metal/Metal.h>
+#import <QuartzCore/CAMetalLayer.h>
+
+#include <iostream>
+
+#include <SDL.h>
+
+const int WIDTH = 800, HEIGHT = 600;
+
+int main()
+{
+    SDL_SetHint(SDL_HINT_RENDER_DRIVER, "metal");
+    SDL_InitSubSystem(SDL_INIT_VIDEO);
+    SDL_Window *window = SDL_CreateWindow("SDL Metal", -1, -1, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
+
+    MTLClearColor color = MTLClearColorMake(0, 0, 0, 1);
+    bool quit = false;
+    SDL_Event e;
+
+    const CAMetalLayer *swapchain = (__bridge CAMetalLayer *)SDL_RenderGetMetalLayer(renderer);
+    const id<MTLDevice> gpu = swapchain.device;
+    const id<MTLCommandQueue> queue = [gpu newCommandQueue];
+    
+    while (!quit) {
+        while (SDL_PollEvent(&e) != 0) {
+            switch (e.type) {
+                case SDL_QUIT: quit = true; break;
+            }
+        }
+
+        id<CAMetalDrawable> surface = [swapchain nextDrawable];
+        
+        color.red = (color.red > 1.0) ? 0 : color.red + 0.01;
+
+        MTLRenderPassDescriptor *pass = [MTLRenderPassDescriptor renderPassDescriptor];
+        pass.colorAttachments[0].clearColor = color;
+        pass.colorAttachments[0].loadAction  = MTLLoadActionClear;
+        pass.colorAttachments[0].storeAction = MTLStoreActionStore;
+        pass.colorAttachments[0].texture = surface.texture;
+
+        id<MTLCommandBuffer> buffer = [queue commandBuffer];
+        id<MTLRenderCommandEncoder> encoder = [buffer renderCommandEncoderWithDescriptor:pass];
+        [encoder endEncoding];
+        [buffer presentDrawable:surface];
+        [buffer commit];
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    
+    return EXIT_SUCCESS;
+}
